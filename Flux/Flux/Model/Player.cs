ec1e3bc040c916eab10681f92dbd1143d5d1f45a
headdetect <headdetective2@gmail.com>
2012-09-04 09:52:48 -0600

    Fiziks

---
+++ Flux/Flux/Model/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Flux.Model.Sprites;
+
+namespace Flux.Model {
+    public class Player {
+
+        public Sprite PlayerSprite { get; set; }
+
+
+
+    }
+}
--- Flux/Flux/Model/Sprites/BallSprite.cs
+++ Flux/Flux/Model/Sprites/BallSprite.cs
@@ -12,67 +12,63 @@ namespace Flux.Model.Sprites {
 
     public class BallSprite : Sprite {
 
-        readonly static Vector2 DEFAULT_SIZE = new Vector2( 50, 50 );
+        readonly static Vector2 DEFAULT_SIZE = new Vector2 ( 50, 50 );
 
         private readonly Vector2 spawnPos;
 
         public BallSprite ( FluxGame fluxGame )
-            : base( fluxGame, DEFAULT_SIZE ) { }
+            : base ( fluxGame, DEFAULT_SIZE ) { }
 
         public BallSprite ( FluxGame fluxGame, Vector2 spawnPos )
-            : base( fluxGame, DEFAULT_SIZE, spawnPos ) {
-                this.spawnPos = spawnPos;
-                this.Body = BodyFactory.CreateCircle( Flux.PhysicsWorld, 50 / Utils.PixelsToMeterRatio, 1f, spawnPos / Utils.PixelsToMeterRatio );
-                this.Body.BodyType = FarseerPhysics.Dynamics.BodyType.Dynamic;
+            : base ( fluxGame, DEFAULT_SIZE, spawnPos ) {
+            this.spawnPos = spawnPos;
+            this.Body = BodyFactory.CreateCircle ( Flux.PhysicsWorld, 50 / Utils.PixelsToMeterRatio, 1f, spawnPos / Utils.PixelsToMeterRatio );
+            this.Body.BodyType = FarseerPhysics.Dynamics.BodyType.Dynamic;
+            this.Body.Friction = 10f;
+            this.Body.Mass = 10f;
 
         }
 
         private float spinIterations = 0;
 
         internal override void Update ( GameTime gameTime ) {
-            KeyboardState state = Keyboard.GetState();
+            KeyboardState state = Keyboard.GetState ();
 
-            /*if ( state.IsKeyDown( Keys.Enter ) ) {
-                this.Position = spawnPos;
+            if ( state.IsKeyDown ( Keys.Enter ) ) {
+                this.Body.ResetDynamics ();
+                this.Body.Position = spawnPos / 64;
                 this.spinIterations = 0;
-                this.Rotation = 0;
+                this.Body.Rotation = 0;
                 return;
             }
 
-            int speedModifier = state.IsKeyDown( Keys.LeftShift ) ? 3 : 1;
+            int speedModifier = state.IsKeyDown ( Keys.LeftShift ) ? 3 : 1;
 
-            if ( state.IsKeyDown( Keys.W ) ) {
-                this.Position -= new Vector2( 0, 3 * speedModifier );
-            }
-            else if ( state.IsKeyDown( Keys.S ) ) {
-                this.Position += new Vector2( 0, 3 * speedModifier );
-            }
-            else if ( state.IsKeyDown( Keys.A ) ) {
-                this.Position -= new Vector2( 3 * speedModifier, 0 );
-                this.Rotation -= 5 * speedModifier;
-            }
-            else if ( state.IsKeyDown( Keys.D ) ) {
-                this.Position += new Vector2( 3 * speedModifier, 0 );
-                this.Rotation += 5 * speedModifier;
-            }
-
-            if ( state.IsKeyDown( Keys.Space ) ) {
+            if ( state.IsKeyDown ( Keys.Space ) ) {
                 spinIterations += .3f;
                 if ( spinIterations > 80 ) {
                     spinIterations = 80;
                 }
-            }
-            else {
+
+            } else {
                 spinIterations -= .5f;
                 if ( spinIterations < 0 ) {
                     spinIterations = 0;
                 }
 
-                this.Position += new Vector2( spinIterations / 2, 0 );
+                //this.Body.ApplyLinearImpulse ( new Vector2 ( spinIterations / 2, 0 ) / 64 );
+
+                if ( state.IsKeyDown ( Keys.A ) ) {
+                    this.Body.ApplyForce ( new Vector2 (-2 * speedModifier, 0 ) / 64 );
+                    //this.Body.ApplyAngularImpulse ( -speedModifier );
+                } else if ( state.IsKeyDown ( Keys.D ) ) {
+                    this.Body.ApplyForce ( new Vector2 ( 2 * speedModifier, 0 ) / 64 );
+                    //this.Body.ApplyAngularImpulse ( speedModifier );
+                }
+
             }
 
-            Rotation += spinIterations;*/
-
+            this.Body.SetTransform ( this.Body.Position, this.Body.Rotation + spinIterations );
             this.Rotation = Body.Rotation;
             this.Position = Body.Position * Utils.PixelsToMeterRatio;
 
